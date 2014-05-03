aa69eb526179563b5b607871b0ffe5abe316b100
angry-cellophane <kazakov.a.s.90@gmail.com>
2014-05-03 22:39:20 +0400

    Added draft List class and its tests

---
+++ src/test/java/com/pds/list/ListTest.java
@@ -0,0 +1,123 @@
+package com.pds.list;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class ListTest {
+
+    @Test
+    public void testSize() throws Exception {
+
+    }
+
+    @Test
+    public void testIsEmpty() throws Exception {
+
+    }
+
+    @Test
+    public void testContains() throws Exception {
+
+    }
+
+    @Test
+    public void testIterator() throws Exception {
+
+    }
+
+    @Test
+    public void testToArray() throws Exception {
+
+    }
+
+    @Test
+    public void testToArray1() throws Exception {
+
+    }
+
+    @Test
+    public void testAdd() throws Exception {
+
+    }
+
+    @Test
+    public void testRemove() throws Exception {
+
+    }
+
+    @Test
+    public void testContainsAll() throws Exception {
+
+    }
+
+    @Test
+    public void testAddAll() throws Exception {
+
+    }
+
+    @Test
+    public void testAddAll1() throws Exception {
+
+    }
+
+    @Test
+    public void testRemoveAll() throws Exception {
+
+    }
+
+    @Test
+    public void testRetainAll() throws Exception {
+
+    }
+
+    @Test
+    public void testClear() throws Exception {
+
+    }
+
+    @Test
+    public void testGet() throws Exception {
+
+    }
+
+    @Test
+    public void testSet() throws Exception {
+
+    }
+
+    @Test
+    public void testAdd1() throws Exception {
+
+    }
+
+    @Test
+    public void testRemove1() throws Exception {
+
+    }
+
+    @Test
+    public void testIndexOf() throws Exception {
+
+    }
+
+    @Test
+    public void testLastIndexOf() throws Exception {
+
+    }
+
+    @Test
+    public void testListIterator() throws Exception {
+
+    }
+
+    @Test
+    public void testListIterator1() throws Exception {
+
+    }
+
+    @Test
+    public void testSubList() throws Exception {
+
+    }
+}
